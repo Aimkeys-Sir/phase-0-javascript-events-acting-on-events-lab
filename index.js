fdee5739162732df86708cdb3650265b0e67fd44
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-06-14 19:42:26 +0300

    commit tree 704fc4597d312e5b5c59f99f8ab7a1e84fad39f0

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+const dodger=document.getElementById("dodger");
+dodger.style.backgroundColor="#ff69b4";
+dodger.style.left="0px";
+document.addEventListener("keydown",function(event)
+{
+    if(event.key==="ArrowLeft")
+    {
+
+        moveDodgerLeft();
+    }
+    else if(event.key==="ArrowRight")
+    {
+        moveDodgerRight();
+    }
+});
+function moveDodgerLeft()
+{
+    const leftNumbers=dodger.style.left.replace("px","");
+    const left=parseInt(leftNumbers,10);
+    console.log(leftNumbers);
+
+    
+         dodger.style.left=`${left-1}px`;   
+    
+
+}
+function moveDodgerRight()
+{
+    const leftNumbers=dodger.style.left.replace("px","");
+    const left=parseInt(leftNumbers,10);
+
+ 
+        dodger.style.left=`${left+1}px`;
+    
+}
